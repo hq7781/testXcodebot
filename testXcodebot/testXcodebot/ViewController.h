a6e86c93d01e3cba069d9b18fe436ae91f73f347
kouken <kouken@BSC0020409.local>
2016-10-13 19:02:49 +0900

    test xcode bot

---
+++ testXcodebot/testXcodebot/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testXcodebot
+//
+//  Created by kouken on 2016/10/13.
+//  Copyright © 2016年 kouken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
